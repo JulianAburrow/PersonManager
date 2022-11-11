6f2ffc11cf0ff09d8074b31f91279f1b8f0b298d
Julian Aburrow <Julian.Aburrow@powwr.co.uk>
2022-11-11 10:21:29 +0000

    Updated SqlClient Nuget package and did some housekeeping

--- DataAccessLibrary/Configuration/AddressConfiguration.cs
+++ DataAccessLibrary/Configuration/AddressConfiguration.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using DataAccessLibrary.Models;
+﻿using DataAccessLibrary.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
--- DataAccessLibrary/Interfaces/IReminderData.cs
+++ DataAccessLibrary/Interfaces/IReminderData.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using DataAccessLibrary.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataAccessLibrary.Models;
 
 namespace DataAccessLibrary.Interfaces
 {
