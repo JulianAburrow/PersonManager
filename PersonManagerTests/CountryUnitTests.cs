ef59f193f07c0fdffc4bce8a397af67d2dbbed15
Julian Aburrow <Julian.Aburrow@powwr.co.uk>
2023-02-17 11:27:34 +0000

    Restructured project

--- PersonManagerTests/CountryUnitTests.cs
+++ PersonManagerTests/CountryUnitTests.cs
@@ -1,3 +1,7 @@
+
+
+using DataAccessLibrary.Models;
+
 namespace PersonManagerTests
 {
     [TestClass]
@@ -5,24 +9,68 @@ namespace PersonManagerTests
     {
         private readonly PersonManagerContext _context;
 
-        public CountryUnitTests(PersonManagerContext context) =>
+        public CountryUnitTests(PersonManagerContext context)
+        { 
             _context = context;
+        }
+            
 
         // Needs refining to account for no countries returned
         [TestMethod]
-        public async Task GetCountryReturnsACountryWhenValidIdIsProvided()
+        public void GetCountryReturnsACountryWhenValidIdIsProvided()
         {
             // Arrange
+            var firstCountry = _context.Countries.First();
             var cd = new CountryData(_context);
-            var countries = await cd.GetCountries();
-            var firstCountry = countries.First();
 
             // Act
             var country = cd.GetCountry(firstCountry.CountryId);
             
-
             // Assert
             Assert.IsTrue(country != null);
         }
+
+        [TestMethod]
+        public async Task GetCountryDoesNotReturnACountryWhenInvalidIdIsProvided()
+        {
+            // Arrange
+            var cd = new CountryData(_context);
+
+            // Act
+            var country = await cd.GetCountry(1000000);
+
+            // Assert
+            Assert.IsTrue(country == null);
+        }
+
+        [TestMethod]
+        public async Task GetCountriesReturnsAllCountries()
+        {
+            // Arrange
+            var cd = new CountryData(_context);
+            var countryCount = _context.Countries.Count();
+
+            // Act
+            var allCountries = await cd.GetCountries();
+
+            // Assert
+            Assert.IsTrue(countryCount == allCountries.Count);
+        }
+
+    //    [TestMethod]
+    //    public async Task AddCountryAddsCountry()
+    //    {
+    //        // Arrange
+    //        var cd = new CountryData(_context);
+    //        var newCountry = new CountryModel
+    //        {
+    //            CountryName = "TestCountry"
+    //        };
+
+
+    //        // Act
+
+
+    //        // Assert
     }
 }
